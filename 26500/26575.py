21c0f55885ad1f1a156ae5dc2976f0391237c4c9
koo_mac <rnxogud136@gmail.com>
2023-01-11 02:35:09 +0900

    implement

--- 26500/26575.py
+++ 26500/26575.py
@@ -4,4 +4,4 @@ read = sys.stdin.readline
 
 for _ in range(int(read())):
     a, b, c = map(float, read().split())
-    print('$', '{:.2f}'.format(round(a * b * c, 2)))
+    print('$', '{:.2f}'.format(round(a * b * c, 2)), sep='')
