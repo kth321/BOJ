33ba9aeb3832f4a6d7052db5706ede0e24037a67
koo_mac <rnxogud136@gmail.com>
2023-01-11 15:32:12 +0900

    implement

--- 06800/6841.py
+++ 06800/6841.py
@@ -2,32 +2,21 @@ import sys
 
 read = sys.stdin.readline
 
+table = {'CU': 'see you',
+        ':-)': 'I’m happy',
+        ':-(': 'I’m unhappy',
+        ';-)': 'wink',
+        ':-P': 'stick out my tongue',
+        '(~.~)': 'sleepy',
+        'TA': 'totally awesome',
+        'CCC': 'Canadian Computing Competition',
+        'CUZ': 'because',
+        'TY': 'thank-you',
+        'YW': "you’re welcome",
+        'TTYL':	'talk to you later'}
+
 while True:
-    s = read().rstrip()
-    if s == "CU":
-        print("see you")
-    elif s == ":-)":
-        print("I'm happy")
-    elif s == ":-(":
-        print("I'm unhappy")
-    elif s == ";-)":
-        print("wink")
-    elif s == ":-P":
-        print("stick out of my tongue")
-    elif s == "(~.~)":
-        print("sleepy")
-    elif s == "TA":
-        print("totally awesome")
-    elif s == "CCC":
-        print("Canadian Computing Competition")
-    elif s == "CUZ":
-        print("because")
-    elif s == "TY":
-        print("thank-you")
-    elif s == "YW":
-        print("you're welcome")
-    elif s == "TTYL":
-        print("talk to you later")
+    s = read().strip()
+    print(table[s] if s in table else s)
+    if s == 'TTYL':
         break
-    else:
-        print(s)
