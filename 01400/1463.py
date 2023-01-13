6883723f41957390cdf797a122968fafabac95d7
koo_mac <rnxogud136@gmail.com>
2023-01-14 06:21:44 +0900

    dp

--- 01400/1463.py
+++ 01400/1463.py
@@ -1,25 +1,11 @@
-num = [0, 0]
+n = int(input())
+dp = [0, 0, 1, 1] + [0] *  (n - 2)
 
-x = int(input())
-num = num + [None] * (x - 1)
-for i in range(2, x + 1):
-    tmp = i
-    cnt = 0
-    while i != 1:
-        if num[i] is not None:
-            cnt += num[i]
-            break
-        if i % 3 == 0:
-            i = i // 3
-        else:
-            if i % 2 == 0:
-                if num[i//2] > num[i-1]:
-                    i -= 1
-                else:
-                    i = i // 2
-            else:
-                i -= 1 
-        cnt += 1
-    num[tmp] = cnt
-print(num[-1])
+for i in range(4, n + 1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
 
+print(dp[n])
