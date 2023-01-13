14b9978e240e89c58b8db1f594e6c6488e2f9f5c
koo_mac <rnxogud136@gmail.com>
2023-01-14 06:43:38 +0900

    dp

---
+++ 02500/2579.py
@@ -0,0 +1,8 @@
+n = int(input())
+stairs = [0] +  [int(input()) for _ in range(n)] + [0]
+dp = [0] * (n + 2)
+dp[1], dp[2] = stairs[1], stairs[1] + stairs[2]
+
+for i in range(3, n + 1):
+    dp[i] = max(stairs[i] + stairs[i-1] + dp[i-3], stairs[i] + dp[i-2])
+print(dp)
