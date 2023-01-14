22db09b4daed3981469bbd86e27d848746e24255
koo_mac <rnxogud136@gmail.com>
2023-01-14 16:37:42 +0900

    brute force

---
+++ 01200/1233.py
@@ -0,0 +1,10 @@
+dice = [0] * 81
+
+s = list(map(int, input().split()))
+
+for i in range(1, s[0] + 1):
+    for j in range(1, s[1] + 1):
+        for k in range(1, s[2] + 1):
+            dice[i + j + k] += 1
+print(dice)
+print(max(range(81), key=lambda i: dice[i]))
