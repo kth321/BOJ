fda76ee1bc6819c643a1c2a4fbb6d9c07c2099ad
koo_mac <rnxogud136@gmail.com>
2023-01-14 17:10:58 +0900

    brute force

--- 01200/1233.py
+++ 01200/1233.py
@@ -6,5 +6,4 @@ for i in range(1, s[0] + 1):
     for j in range(1, s[1] + 1):
         for k in range(1, s[2] + 1):
             dice[i + j + k] += 1
-print(dice)
 print(max(range(81), key=lambda i: dice[i]))
