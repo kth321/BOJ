229ef0a3932773791953731d31bdf6ce99613846
koo_mac <rnxogud136@gmail.com>
2023-01-19 02:15:37 +0900

    bfs

--- 02100/2178.py
+++ 02100/2178.py
@@ -5,24 +5,22 @@ read = sys.stdin.readline
 
 directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
+def bfs(i=0, j=0):
+    q = deque()
+    q.append((i, j))
+    while q:
+        x, y = q.popleft()
+        for d in directions:
+            nx, ny = x + d[0], y + d[1]
+            if 0 <= nx < n and 0 <= ny < m and \
+                dist[nx][ny] == 0 and graph[nx][ny] == '1':
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx, ny))
+
+
 n, m = map(int, read().split())
-maze = [list(read()) for _ in range(n)]
-check = [[False] * m for _ in range(n)]
+graph = [list(read().rstrip()) for _ in range(n)]
 dist = [[0] * m for _ in range(n)]
-q = deque()
-
-check[0][0] = True
 dist[0][0] = 1
-q.append((0, 0))
-
-while q:
-    x, y = q.popleft()
-    for d in directions:
-        nx, ny = x + d[0], y + d[1]
-        if 0 <= nx < n and 0 <= ny < m and \
-            check[nx][ny] == False and maze[nx][ny] == '1':
-            check[nx][ny] = True
-            dist[nx][ny] = dist[x][y] + 1
-            q.append((nx, ny))
-
+bfs()
 print(dist[n-1][m-1])
