367127c00dc845bf0f4105c22587456805de7c4b
koo_mac <rnxogud136@gmail.com>
2023-01-19 02:43:22 +0900

    bfs

--- 07500/7576.py
+++ 07500/7576.py
@@ -1,34 +1,34 @@
-from collections import deque
 import sys
+from collections import deque
 
 read = sys.stdin.readline
 
 directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
+def bfs():
+    q = deque()
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                q.append((i, j))
+    
+    while q:
+        x, y = q.popleft()
+        for d in directions:
+            nx, ny = x + d[0], y + d[1]
+            if 0 <= nx < n and 0 <= ny < m and \
+                graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+
+
 m, n = map(int, read().split())
+graph = [list(map(int, read().split())) for _ in range(n)]
 res = 0
-table = [list(map(int, read().split()))
-        for _ in range(n)]
-q = deque()
-
-for i in range(n):
-    for j in range(m):
-        if table[i][j] == 1:
-            q.append((i, j))
-
-while q:
-    x, y = q.popleft()
-    for d in directions:
-        nx, ny = x + d[0], y + d[1]
-        if 0 <= nx < n and 0 <= ny < m and \
-            table[nx][ny] == 0:
-            table[nx][ny] = table[x][y] + 1
-            q.append((nx, ny))
-
-for line in table:
+bfs()
+for line in graph:
     if 0 in line:
         print(-1)
         exit()
     res = max(res, max(line))
-
 print(res - 1)
