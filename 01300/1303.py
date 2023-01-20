e187c7c6553018fda67d08def205da77d0dd8f8c
koo_mac <rnxogud136@gmail.com>
2023-01-20 18:54:46 +0900

    bfs

--- 01300/1303.py
+++ 01300/1303.py
@@ -7,33 +7,28 @@ directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
 def bfs(i, j, c):
     q = deque()
-    q.append((i, j))
     cnt = 0
+    q.append((i, j))
     while q:
         x, y = q.popleft()
         if 0 <= x < m and 0 <= y < n and \
-            visited[x][y] is False and table[x][y] == c:
-            visited[x][y] = True
+            graph[x][y] == c and visited[x][y] == False:
             cnt += 1
+            visited[x][y] = True
             for d in directions:
                 nx, ny = x + d[0], y + d[1]
                 q.append((nx, ny))
     return cnt
 
 n, m = map(int, read().split())
-table = [list(read().rstrip()) for _ in range(m)]
+graph = [list(read().rstrip()) for _ in range(m)]
 visited = [[False] * n for _ in range(m)]
 w, b = 0, 0
 
 for i in range(m):
     for j in range(n):
-        print(table[i][j], end=' ')
-    print()
-
-for i in range(m):
-    for j in range(n):
         if visited[i][j] == False:
-            if table[i][j] == 'W':
+            if graph[i][j] == 'W':
                 w += bfs(i, j, 'W') ** 2
             else:
                 b += bfs(i, j, 'B') ** 2
