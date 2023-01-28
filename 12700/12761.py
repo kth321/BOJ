376da494a31cc11d555c3cef5c8044b711d88700
koo_mac <rnxogud136@gmail.com>
2023-01-29 00:02:16 +0900

    bfs

--- 12700/12761.py
+++ 12700/12761.py
@@ -4,7 +4,8 @@ from collections import deque
 read = sys.stdin.readline
 
 a, b, n, m = map(int, read().split())
-graph = [0] * 10 ** 5
+S = 10 ** 5 + 1
+graph = [0] * S
 q = deque([n])
 while q:
     if graph[m] != 0:
@@ -16,15 +17,15 @@ while q:
         graph[v - 1] = graph[v] + 1
         q.append(v - 1)
 
-    if v + 1 < 10 ** 5 and graph[v + 1] == 0:
+    if v + 1 < S and graph[v + 1] == 0:
         graph[v + 1] = graph[v] + 1
         q.append(v - 1)
 
-    if v + a < 10 ** 5 and graph[v + a] == 0:
+    if v + a < S and graph[v + a] == 0:
         graph[v + a] = graph[v] + 1
         q.append(v + a)
 
-    if v + b < 10 ** 5 and graph[v + b] == 0:
+    if v + b < S and graph[v + b] == 0:
         graph[v + b] = graph[v] + 1
         q.append(v + b)
 
@@ -36,10 +37,10 @@ while q:
         graph[v - b] = graph[v] + 1
         q.append(v - b)
 
-    if 0 <= a * v < 10 ** 5 and graph[a * v] == 0:
+    if 0 <= a * v < S and graph[a * v] == 0:
         graph[a * v] = graph[v] + 1
         q.append(a * v)
 
-    if 0 <= b * v < 10 ** 5 and graph[b * v ] == 0:
+    if 0 <= b * v < S and graph[b * v ] == 0:
         graph[b * v] = graph[v] + 1
         q.append(b * v)
