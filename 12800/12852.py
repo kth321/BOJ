ae256740fb942021ca1bf92f9227121976078106
koo_mac <rnxogud136@gmail.com>
2023-02-02 16:12:35 +0900

    dp

--- 12800/12852.py
+++ 12800/12852.py
@@ -1,24 +1,39 @@
 import sys
-from collections import deque
+max_v = sys.maxsize
 
+n = int(input())
+dp = [[0, max_v] for _ in range(n+1)]
+res = []
+dp[1][1] = 0
 
-def bfs(v):
-    q = deque([[v]])
-    while q:
-        target = q.popleft()
-        temp = target[0]
-        print(temp)
-        if temp == 1:
-            return target
-        if temp % 3 == 0:
-            q.append([temp // 3] + target)
-        if temp % 2 == 0:
-            q.append([temp // 2] + target)
-        q.append([temp - 1] + target)
+for i in range(1, n):
+    if i + 1 < n + 1 and dp[i+1][1] > dp[i][1] + 1:
+        dp[i+1] = [i, dp[i][1] + 1]
+    if i * 2 < n + 1 and dp[i*2][1] > dp[i][1] + 1:
+        dp[i*2] = [i, dp[i][1] + 1]
+    if i * 3 < n + 1 and dp[i*3][1] > dp[i][1] + 1:
+        dp[i*3] = [i, dp[i][1] + 1]
 
-
-n = int(sys.stdin.readline())
-res = bfs(n)
-
+while n >= 1:
+    res.append(n)
+    n = dp[n][0]
 print(len(res) - 1)
-print(res)
+print(*res)
+
+# n = int(input())
+# dp = [[0, []] for _ in range(n + 1)]
+# dp[1][0] = 0
+# dp[1][1] = [1]
+
+# for i in range(2, n + 1):
+#     dp[i][0] = dp[i-1][0] + 1
+#     dp[i][1] = dp[i-1][1] + [i]
+#     if i % 3 == 0 and dp[i // 3][0] + 1 < dp[i][0]:
+#         dp[i][0] = dp[i // 3][0] + 1
+#         dp[i][1] = dp[i // 3][1] + [i]
+#     if i % 2 == 0 and dp[i // 2][0] + 1 < dp[i][0]:
+#         dp[i][0] = dp[i // 2][0] + 1
+#         dp[i][1] = dp[i // 2][1] + [i]
+
+# print(len(dp[n][1]) - 1)
+# print(*reversed(dp[n][1]))
