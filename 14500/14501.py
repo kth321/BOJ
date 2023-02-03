ce2d60706f8767be688f0e2b6322574b6dae1ac6
koo_mac <rnxogud136@gmail.com>
2023-02-03 23:06:13 +0900

    dp

--- 14500/14501.py
+++ 14500/14501.py
@@ -1,10 +1,13 @@
 import sys
 
 read = sys.stdin.readline
+n = int(input())
+pay = [0] + [list(map(int, read().split())) for _ in range(n)]
+dp = [0] * (n + 2)
 
-n = int(read())
-tp = [tuple(map(int, read().split())) for _ in range(n)]
-dp = [0] * n
-
-for i in range(n):
-    dp[i + tp[i][0]] = max(dp[i + tp[i][0]], tp[i][1])
+for i in range(1, n + 1):
+    t, p = pay[i]
+    dp[i+1] = max(dp[i+1], dp[i])
+    if i + t < n + 2:
+        dp[i + t] = max(dp[i + t], dp[i] + p)
+print(dp[n+1])
