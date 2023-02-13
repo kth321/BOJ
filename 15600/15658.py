c02bcd67020f493f24f3a590de8496a093a5b170
koo_mac <rnxogud136@gmail.com>
2023-02-13 12:13:51 +0900

    back tracking

---
+++ 15600/15658.py
@@ -0,0 +1,23 @@
+def dfs(idx, res, plus, minus, mul, div):
+    global max_v, min_v
+    if idx == n:
+        max_v = max(max_v, res)
+        min_v = min(min_v, res)
+        return None
+    if plus:
+        dfs(idx+1, res + nums[idx], plus-1, minus, mul, div)
+    if minus:
+        dfs(idx+1, res - nums[idx], plus, minus-1, mul, div)
+    if mul:
+        dfs(idx+1, res * nums[idx], plus, minus, mul-1, div)
+    if div:
+        dfs(idx+1, int(res / nums[idx]), plus, minus, mul, div-1)
+
+n = int(input())
+nums = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+v = 1e9
+max_v, min_v = -v, v
+dfs(1, nums[0], plus, minus, mul, div)
+print(max_v)
+print(min_v)
