8d9079bee013652ebde1f041cd46f186e9ecb59d
koo_mac <rnxogud136@gmail.com>
2023-02-20 03:08:24 +0900

    devide&conquer

---
+++ 02600/2630.py
@@ -0,0 +1,31 @@
+import sys
+
+read = sys.stdin.readline
+
+def sol(i, j, n):
+    if n == 1:
+        if graph[i][j] == 0:
+            global w
+            w += 1
+        else:
+            global b
+            b += 1
+        return None
+    if all(graph[a][b] for a in range(i, i+n)
+                        for b in range(j, j+n)):
+        b += 1
+        return None
+    if all(graph[a][b] == 0 for a in range(i, i+n)
+                        for b in range(j, j+n)):
+        w += 1
+        return None
+    sol(i, j, n // 2)
+    sol(i + n//2, j, n // 2)
+    sol(i, j + n//2, n // 2)
+    sol(i + n//2, j + n//2, n // 2)
+
+n = int(read())
+graph = [list(map(int, read().split())) for _ in range(n)]
+w, b = 0, 0
+sol(0, 0, n)
+print(w, b)
