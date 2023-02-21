6553308fdb099f3ab1db6c80277ab311fdc16acb
koo_mac <rnxogud136@gmail.com>
2023-02-21 15:10:07 +0900

    greedy

---
+++ 01300/1339.py
@@ -0,0 +1,22 @@
+import sys
+from collections import defaultdict
+
+read = sys.stdin.readline
+
+n = int(read())
+nums = [list(read().rstrip()) for _ in range(n)]
+table = defaultdict(int)
+res, idx = 0, 9
+
+for num in nums:
+    square_root = len(num) - 1
+    for ch in num:
+        table[ch] += 10 ** square_root
+        square_root -= 1
+res_list = sorted(table.values(), reverse=True)
+for elem in res_list:
+    res += elem * idx
+    idx -= 1
+print(table)
+print(res_list)
+print(res)
