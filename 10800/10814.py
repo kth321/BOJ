d8c3b458dbd110f33517704d0007e6ca4e42120e
koo_mac <rnxogud136@gmail.com>
2023-02-21 18:24:15 +0900

    sort

--- 10800/10814.py
+++ 10800/10814.py
@@ -1,35 +1,10 @@
-class P:
-    def __init__(self, info):
-        self.age, self.name = info.split()
-    def _print(self):
-        print(f'{self.age} {self.name}')
+import sys
 
-def ms(s):
-    if len(s) < 2:
-        return s
-    m = len(s) // 2
-    l, r = s[:m], s[m:]
-    if len(l) > 1:
-        l = ms(l)
-    if len(r) > 1:
-        r = ms(r)
-    res = []
-    i, j = 0, 0
-    while i < len(l) and j < len(r):
-        if l[i].age > r[j].age:
-            res.append(r[j])
-            j += 1
-        elif l[i].age <= r[j].age:
-            res.append(l[i])
-            i += 1
-    if l[i:]:
-        res.extend(l[i:])
-    if r[j:]:
-        res.extend(r[j:])
-    return res
+read = sys.stdin.readline
 
-L = []
-for _ in range(int(input())):
-    L.append(P(input()))
-for i in ms(L):
-    i._print()
+nums = []
+for i in range(int(read())):
+    nums.append([i] + list(read().split()))
+    nums[i][1] = int(nums[i][1])
+for num in sorted(nums, key=lambda x: (x[1], x[0])):
+    print(*num[1:])
