106eaf8d6b452e3befb1b9c42a46a4e4f2960e1e
koo_mac <rnxogud136@gmail.com>
2023-02-22 19:23:01 +0900

    deque

--- 05400/5430.py
+++ 05400/5430.py
@@ -4,4 +4,29 @@ import sys
 read = sys.stdin.readline
 
 for _ in range(int(read())):
-    pass
+    command = read().rstrip()
+    n = read().rstrip()
+    nums = read().rstrip()[1:-1].split(',')
+    R = False
+    e_flag = False
+
+    nums = deque(nums)
+    if n == '0':
+        nums = deque()
+    for s in command:
+        if s == 'R':
+            if R: R = False
+            else: R = True
+        else:
+            if not nums:
+                print('error')
+                e_flag = True
+                break
+            if R:
+                nums.pop()
+            else:
+                nums.popleft()
+    if not e_flag:
+        if R:
+            nums = reversed(nums)
+        print('[' + ','.join(nums) + ']')
