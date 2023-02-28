77635e2e5837de97ee4a084e35e78126ea5823bd
koo_mac <rnxogud136@gmail.com>
2023-03-01 08:14:26 +0900

    floyd-warshall

--- 11400/11404.py
+++ 11400/11404.py
@@ -1,7 +1,7 @@
 import sys
 
 read = sys.stdin.readline
-INF = int(1e5)
+INF = int(1e9)
 
 n, m = int(read()), int(read())
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
@@ -20,5 +20,7 @@ for k in range(1, n + 1):
         for j in range(1, n + 1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-for line in graph[1:]:
-    print(*line[1:])
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(graph[i][j] if graph[i][j] != INF else 0, end=' ')
+    print()
